8a72710cd02e96c838f1e482f06571f8c76c9aed
Mariah A. Knowles <snotskie@gmail.com>
2024-05-30 09:23:44 -0500

    updating to match general template tweaks in STC

--- site/make.jl
+++ site/make.jl
@@ -1,4 +1,14 @@
 using Documenter
 
-makedocs(sitename="DocumenterSiteTemplate")
+makedocs(
+    sitename="DocumenterSiteTemplate",
+    format=Documenter.HTML(
+        assets=[
+            "assets/style.css",
+            "assets/script.js"
+        ],
+        footer="© 2024 The Board of Regents of the University of Wisconsin System"
+    )
+)
+
 deploydocs(repo="github.com/Tiny-Earth/DocumenterSiteTemplate.git")
---
+++ site/src/assets/script.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function(){
+    let prevref = "";
+    for (const a of document.body.getElementsByTagName("a")){
+        // Open external links in new tabs
+        if (a.href.length > 0 && new URL(a.href).origin !== location.origin){
+            a.setAttribute("target", "_blank");
+        }
+
+        // Replace identical subsequent references with "id."
+        if (a.href.length > 0 && new URL(a.href).pathname.endsWith("/refs/")){
+            if (prevref === a.innerText){
+                a.innerHTML = "<em>id.</em>";
+            } else {
+                prevref = a.innerText;
+            }
+        }
+    }
+
+    // Reword "search docs"
+    document.getElementById("documenter-search-query").innerText = "Search";
+});
